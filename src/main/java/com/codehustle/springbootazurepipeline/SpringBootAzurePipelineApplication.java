483e943e2944852c791782ac51aa46f18272a12e
codehustle98 <codehustle98@gmail.com>
2024-05-30 09:56:05 +0530

    initial commit

---
+++ src/main/java/com/codehustle/springbootazurepipeline/SpringBootAzurePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.codehustle.springbootazurepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzurePipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAzurePipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codehustle/springbootazurepipeline/SpringBootAzurePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehustle.springbootazurepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzurePipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
